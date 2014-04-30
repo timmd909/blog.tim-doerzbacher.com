b3aaba314d9f999cff9c08c1dbfc200d06bc5310
Tim Doerzbacher <tim@tim-doerzbacher.com>
2014-04-30 21:46:07 +0000

    Commiting updates from auto update

--- www/wp-content/plugins/photo-gallery/frontend/models/BWGModelGalleryBox.php
+++ www/wp-content/plugins/photo-gallery/frontend/models/BWGModelGalleryBox.php
@@ -41,7 +41,7 @@ class BWGModelGalleryBox {
     return $row;
   }
 
-  public function get_image_rows_data($gallery_id, $sort_by) {
+  public function get_image_rows_data($gallery_id, $sort_by, $order_by = 'asc') {
     global $wpdb;
     if ($sort_by == 'size' || $sort_by == 'resolution') {
       $sort_by = ' CAST(' . $sort_by . ' AS SIGNED) ';
@@ -49,13 +49,19 @@ class BWGModelGalleryBox {
     elseif (($sort_by != 'alt') && ($sort_by != 'date') && ($sort_by != 'filetype')) {
       $sort_by = '`order`';
     }
-    $row = $wpdb->get_results($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'bwg_image WHERE published=1 AND gallery_id="%d" ORDER BY ' . $sort_by . ' ASC', $gallery_id));
+    $row = $wpdb->get_results($wpdb->prepare('SELECT * FROM ' . $wpdb->prefix . 'bwg_image WHERE published=1 AND gallery_id="%d" ORDER BY ' . $sort_by . ' ' . $order_by, $gallery_id));
     return $row;
   }
 
-  public function get_image_rows_data_tag($tag_id, $sort_by) {
+  public function get_image_rows_data_tag($tag_id, $sort_by, $order_by = 'asc') {
     global $wpdb;
-    $row = $wpdb->get_results($wpdb->prepare('SELECT image.* FROM ' . $wpdb->prefix . 'bwg_image as image INNER JOIN ' . $wpdb->prefix . 'bwg_image_tag as tag ON image.id=tag.image_id WHERE image.published=1 AND tag.tag_id="%d" ORDER BY `' . $sort_by . '` ASC', $tag_id));
+    if ($sort_by == 'size' || $sort_by == 'resolution') {
+      $sort_by = ' CAST(' . $sort_by . ' AS SIGNED) ';
+    }
+    elseif (($sort_by != 'alt') && ($sort_by != 'date') && ($sort_by != 'filetype')) {
+      $sort_by = '`order`';
+    }
+    $row = $wpdb->get_results($wpdb->prepare('SELECT image.* FROM ' . $wpdb->prefix . 'bwg_image as image INNER JOIN ' . $wpdb->prefix . 'bwg_image_tag as tag ON image.id=tag.image_id WHERE image.published=1 AND tag.tag_id="%d" ORDER BY ' . $sort_by . ' ' . $order_by, $tag_id));
     return $row;
   }
 
