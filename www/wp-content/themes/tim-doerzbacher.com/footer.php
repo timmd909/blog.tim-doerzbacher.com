21dbfa402d85be610a74104bb477cd1f008c3594
Tim Doerzbacher <tim@tim-doerzbacher.com>
2015-11-02 23:57:00 -0500

    Update blog skinning

--- www/wp-content/themes/tim-doerzbacher.com/footer.php
+++ www/wp-content/themes/tim-doerzbacher.com/footer.php
@@ -9,15 +9,15 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		</div><!-- / .container -->
+	</main><!-- /main -->
+
+
+	<footer class="footer-main">
+		<p>&copy; 2015 Tim Doerzbacher &mdash; All Rights Reserved</p>
+	</footer>
 
 <?php wp_footer(); ?>
+
 </body>
 </html>
