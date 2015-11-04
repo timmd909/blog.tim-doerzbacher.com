656dabd86b8301c8c7ceaf8ec7ab629e0642e133
Tim Doerzbacher <tim@tim-doerzbacher.com>
2015-11-04 03:10:56 -0500

    Finish up Bootstrap skinning
    There will be tweak in the future... but this is close enough
    to make me happy.

--- www/wp-includes/general-template.php
+++ www/wp-includes/general-template.php
@@ -230,23 +230,17 @@ function get_search_form( $echo = true ) {
 		require( $search_form_template );
 		$form = ob_get_clean();
 	} else {
-		if ( 'html5' == $format ) {
-			$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
-				<label>
-					<span class="screen-reader-text">' . _x( 'Search for:', 'label' ) . '</span>
-					<input type="search" class="search-field" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label' ) . '" />
+		$form = '<form role="search" method="get" class="search-form" action="' . esc_url( home_url( '/' ) ) . '">
+			<div class="form-group">
+				<label for="search-field">
+					<span class="sr-only">' . _x( 'Search for:', 'label' ) . '</span>
 				</label>
-				<input type="submit" class="search-submit" value="'. esc_attr_x( 'Search', 'submit button' ) .'" />
-			</form>';
-		} else {
-			$form = '<form role="search" method="get" id="searchform" class="searchform" action="' . esc_url( home_url( '/' ) ) . '">
-				<div>
-					<label class="screen-reader-text" for="s">' . _x( 'Search for:', 'label' ) . '</label>
-					<input type="text" value="' . get_search_query() . '" name="s" id="s" />
-					<input type="submit" id="searchsubmit" value="'. esc_attr_x( 'Search', 'submit button' ) .'" />
-				</div>
-			</form>';
-		}
+				<input type="search" id="search-field" class="form-control" placeholder="' . esc_attr_x( 'Search &hellip;', 'placeholder' ) . '" value="' . get_search_query() . '" name="s" title="' . esc_attr_x( 'Search for:', 'label' ) . '" />
+			</div>
+			<div class="form-group">
+				<input type="submit" class="search-submit form-control" value="'. esc_attr_x( 'Search', 'submit button' ) .'" />
+			</div>
+		</form>';
 	}
 
 	/**
